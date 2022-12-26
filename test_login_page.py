12d0a9231e349350ff0d027375f86c4cd3af697b
ap1k86 <ap1k86@yandex.ru>
2022-12-26 23:09:56 +0300

    add new files. test_login_page.py, login_page.py.

--- test_login_page.py
+++ test_login_page.py
@@ -3,7 +3,7 @@ from .pages.login_page import LoginPage
 
 # Метод проверки корректности url адреса.
 def test_quest_should_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_url()
